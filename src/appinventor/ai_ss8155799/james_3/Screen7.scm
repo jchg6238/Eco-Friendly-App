b099770de8ba1f70744bf008da034441f0df044e
jchg6238 <jchg6238@gmail.com>
2020-07-12 15:33:12 +0800

    first upload

---
+++ src/appinventor/ai_ss8155799/james_3/Screen7.scm
@@ -0,0 +1,4 @@
+#|
+$JSON
+{"authURL":["ai2.appinventor.mit.edu"],"YaVersion":"196","Source":"Form","Properties":{"$Name":"Screen7","$Type":"Form","$Version":"26","AccentColor":"&HFFFFFFFF","AppName":"james_1","PrimaryColorDark":"&HFFFFFF00","Title":"Screen7","Uuid":"0","$Components":[{"$Name":"\u7db2\u8def\u700f\u89bd\u56681","$Type":"WebViewer","$Version":"8","Uuid":"-1503844495"}],"Sizing":"Fixed","ShowListsAsJson":"False"}}
+|#
---
+++ src/appinventor/ai_ss8155799/james_3/Screen8.scm
@@ -0,0 +1,4 @@
+#|
+$JSON
+{"authURL":["ai2.appinventor.mit.edu"],"YaVersion":"196","Source":"Form","Properties":{"$Name":"Screen8","$Type":"Form","$Version":"26","AccentColor":"&HFFFFFFFF","AppName":"james_3","PrimaryColorDark":"&HFFFFFF00","Title":"Screen8","Uuid":"0","$Components":[{"$Name":"Map1","$Type":"Map","$Version":"5","Uuid":"140772487"}],"Sizing":"Fixed","ShowListsAsJson":"False"}}
+|#
